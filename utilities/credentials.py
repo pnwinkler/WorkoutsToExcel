cd7150883a716b95ed09b6500162c6a8f3dcf11d
pnwinkler <pwinkl01@dcs.bbk.ac.uk>
2020-06-15 14:34:05 +0100

    Update credentials.py

--- utilities/credentials.py
+++ utilities/credentials.py
@@ -4,4 +4,6 @@
 # Optionally change. If left as None, it will need manual typing each run
 # this is your Google account username
 username = None
+
+# I am unsure that storing the password here is wise. This may well be removed - supposedly there are better alternatives, which I have yet to research
 password = None
