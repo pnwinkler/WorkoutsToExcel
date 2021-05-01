20f78489ff86ce3b710572cb8a0acdb8c95e52d8
Philip Winkler <pwinkl01@dcs.bbk.ac.uk>
2021-05-01 11:24:28 +0200

    Refactoring

--- Bodyweights2Calc/main.py
+++ Bodyweights2Calc/main.py
@@ -4,9 +4,7 @@
 # does intelligent stuff too, like alert the user to missing entries, etc
 # consider creating a version for use by crontab
 # REMEMBER to change utilities.params whenever necessary, and to suffix EVERY bodyweight with a comma.
-
-
-
+import gkeepapi.node
 import openpyxl
 import re
 from datetime import datetime
@@ -59,8 +57,9 @@ def main():
         exit()
 
     bodyweights_lst = return_bw_lst(bw_note)
+    # todo: replace this. You don't want start row determined by what you have. You want it determined by
+    #  what it should be, i.e. what's missing
     start_row = end_row - len(bodyweights_lst) + 1
-    # print(f"DEBUG: start_row={start_row} , end_row={end_row}, bodyweights_lst={bodyweights_lst}")
 
     # pair bodyweights with their target rows, as tpl[0]=int row, tpl[1]=str bodyweight
     # it will raise exceptions if anything is amiss.
@@ -116,29 +115,22 @@ def return_history(bw_note, history_length) -> str:
     return history
 
 
-def trash_original_and_replace(keep, bw_note, history):
+def trash_original_and_replace(keep, bw_note, history) -> None:
     # Trash original bodyweight note, and replace with a new bodyweights note
-    # items in trash remain available for 7 days.
-    # whereas changes to bw_note would be irreversible
+    # items in trash remain available for 7 days, whereas changes to bw_note are irreversible
     # that's why we create a new note this way.
 
-    # no title
-    keep.createNote('', history)
+    keep.createNote(title='', history=history)
     bw_note.trash()
     keep.sync()
     print("Synchronizing")
-    # without a pause sometimes sync doesn't seem to complete
-    # it seems to have failed with a 1 second pause before
+    # without a wait sometimes sync doesn't complete
     import time
     time.sleep(3)
 
 
-def find_bodyweights_note(notes):
-    """
-    Within "notes", find the bodyweights note and return it
-    :param notes: the Keep notes object (which contains all notes)
-    :return: the note containing bodyweights
-    """
+def find_bodyweights_note(notes) -> gkeepapi.node.Note:
+    # Within "notes", find the bodyweights note and return it. Match either title or body.
     # we expect bodyweight note's format to resemble formats like these 3 below:
     # 83.2, 83, ?, 83.4,
     # 101,
@@ -146,20 +138,17 @@ def find_bodyweights_note(notes):
     # i.e. 2-3 digits with optional decimal place, followed by a comma
     # spaces are optional. Commas are not. Each number must be followed by one comma
     for gnote in notes:
-        # by default, trashed notes are also searched.
+        # don't search trashed notes.
         if gnote.trashed:
             continue
 
-        # match either title or body.
         for x in [gnote.title, gnote.text]:
             if x.isdigit() and len(x) > 3:
                 # it's probably a PIN
                 continue
 
-            # parentheses are part of the history / context window.
-            grammar = "(),.? "
-            for symbol in grammar:
-                x = x.replace(symbol, '')
+            for accepted_symbol in ["(),.? "]:
+                x = x.replace(accepted_symbol, '')
             if not x.isdigit():
                 continue
             return gnote
@@ -170,42 +159,41 @@ def find_bodyweights_note(notes):
                      "3) Does it contain only numbers, spaces, commas and full stops?")
 
 
-def return_bodyweights_note_edit_timestamp(bw_note):
-    """
-    :param bw_note: the note containing bodyweights
-    :return: datetime object in form '%Y-%m-%dT%H:%M:%S.%fZ
-    example return value "2020-07-06 11:20:44.428000"
-    """
+def return_bodyweights_note_edit_timestamp(bw_note) -> datetime.date:
+    # Take note containing bodyweights, return datetime object in form %Y-%m-%dT%H:%M:%S.%fZ
+    # example: "2020-07-06 11:20:44.428000"
     return bw_note.timestamps.edited
 
 
-def return_bw_lst(bw_note):
-    # takes bodyweights note, and returns a list of bodyweights
-    # that were found outside of parentheses (i.e. outside of history)
-
+def return_bw_lst(bw_note) -> list:
+    # Take bodyweights note and return list of bodyweights found outside of parentheses
+    # (i.e. outside of history / context window)
     bodyweights = []
     for x in [bw_note.title, bw_note.text]:
         if x.count("(") != x.count(")"):
-            print("ERROR: mismatched parentheses in bodyweights")
-            exit()
+            print("ERROR: mismatched parentheses in bodyweights note")
+            raise ValueError
         if x.count("(") == 1:
             # discard context window, to find uncommitted bodyweights
             # changes "(82.3, 84.5), ?, 85" to " ?, 85"
             x = x.split("),")[1]
+        if x.count("(") > 1 or x.count(")") > 1:
+            print("ERROR: unexpected number of parentheses in bodyweights note")
+            raise ValueError
 
         if len(re.findall(bw_reg, x)) > 0:
-            if len(bodyweights) != 0:
-                print("ERROR: Bodyweights found in both title and text of note")
-                print("Please tidy up note, such that *either* the note's title *or*", end=' ')
-                print("the note's text contain ALL of the bodyweights, and the counterpart is empty")
-                exit()
-            else:
+            if len(bodyweights) == 0:
                 # change findall's output from this kind:
                 # [('', '81,'), ('', '85,'), ('', '102,'), ('102.1,', '')]
                 # to this kind
                 # ['81', '85', '102', '102.1']
                 bodyweights = ["".join(m) for m in re.findall(bw_reg, x)]
                 bodyweights = [t.replace(',', '') for t in bodyweights]
+            else:
+                print("ERROR: Bodyweights found in both title and text of note")
+                print("Please tidy up note, such that *either* the note's title *or*", end=' ')
+                print("the note's text contain ALL of the bodyweights, and the counterpart is empty")
+                raise ValueError
 
     if len(bodyweights) > 0:
         return bodyweights
@@ -217,7 +205,7 @@ def return_bw_lst(bw_note):
         exit()
 
 
-def pair_bodyweights_with_rows(sheet, bodyweights_lst, start_row):
+def pair_bodyweights_with_rows(sheet, bodyweights_lst, start_row: int):
     """
     :param sheet: sheet in xlsx file containing bodyweights and dates
     :param bodyweights_lst: list of bodyweights in such a format: ['81', '85', '102', '102.1']
@@ -235,7 +223,6 @@ def pair_bodyweights_with_rows(sheet, bodyweights_lst, start_row):
 
     for bw in bodyweights_lst:
         date_cell_value = sheet.cell(row=current_row, column=p.date_column).value
-
         while date_cell_value is None:
             # skip empty cells in date column (e.g. at end of year), up to max length "max_empty_rows"
             current_row += 1
@@ -248,11 +235,7 @@ def pair_bodyweights_with_rows(sheet, bodyweights_lst, start_row):
 
         bw_cell_value = sheet.cell(row=current_row, column=p.bodyweight_column).value
         if bw_cell_value is None:
-            try:
-                tpl = (current_row, float(bw))
-            except ValueError:
-                # it's probably a "?"
-                tpl = (current_row, str(bw))
+            tpl = (current_row, str(bw))
             retlist.append(tpl)
             current_row += 1
         else:
@@ -266,10 +249,8 @@ def pair_bodyweights_with_rows(sheet, bodyweights_lst, start_row):
 
 
 def write_to_file(wb, sheet, row_bodyweight_tuple_list):
-    """
-    :param bodyweights_rows_tuple_list: a list of tuples, where tuple[0] is the int row to write to,
-    and tuple[1] the str bodyweight value
-    """
+    # bodyweights_rows_tuple_list is a list of tuples, where tuple[0] is the int row to write to,
+    # and tuple[1] the str bodyweight value)
 
     for tpl in row_bodyweight_tuple_list:
         try:
