9fe846882aa90eb29c61293f96be62fd77eea72a
Philip Winkler <pwinkl01@dcs.bbk.ac.uk>
2021-05-13 13:01:48 +0200

    Add function to count empty row. Also refactored

--- utilities/utility_functions.py
+++ utilities/utility_functions.py
@@ -40,44 +40,65 @@ def convert_ddmmyyyy_to_datetime(date_str, verbose=True):
     #     return -1
 
     try:
-        date_str = datetime.strptime(date_str, '%d%B%Y')
+        datetime_obj = datetime.strptime(date_str, '%d%B%Y')
     except ValueError:
         try:
-            date_str = datetime.strptime(date_str, '%d%b%Y')
+            datetime_obj = datetime.strptime(date_str, '%d%b%Y')
         except ValueError:
             try:
-                date_str = datetime.strptime(date_str, '%B%d%Y')
+                datetime_obj = datetime.strptime(date_str, '%B%d%Y')
             except ValueError:
                 try:
-                    date_str = datetime.strptime(date_str, '%b%d%Y')
+                    datetime_obj = datetime.strptime(date_str, '%b%d%Y')
                 except Exception as e:
                     # Possible causes: UTF 8 bullshit; unconverted data like "day 1"
                     if verbose:
                         print('Error in utilities convert_ddmm_to_datetime:', e)
                     return -1
     now = datetime.now()
-    date_str = date_str.replace(year=now.year)
-    if now < date_str:
+    datetime_obj = datetime_obj.replace(year=now.year)
+    if now < datetime_obj:
         # exercise would be in the future, so we assume it's from last year
-        return date_str.replace(year=now.year - 1)
-    return date_str
+        return datetime_obj.replace(year=now.year - 1)
+    return datetime_obj
 
 
-def find_row_of_datecell_given_datetime(sheet, datetime_date, date_column=2):
+def count_empty_cells_between_rows(sheet, start_row, end_row, cols_lst: list):
+    # a non-inclusive count. Given target sheet, start and end rows, and a simple or composite key, counts how many
+    # rows between the 2 passed in rows have empty values in the key columns
+
+    if isinstance(cols_lst, str):
+        cols_lst = list(cols_lst)
+    cols = [int(x) for x in cols_lst]
+
+    count = 0
+    for r in range(start_row + 1, end_row):
+        for col in cols:
+            if not sheet.cell(row=r, column=col).value:
+                count += 1
+                break
+    return count
+
+
+def find_row_of_datecell_given_datetime(sheet, datetime_target, date_column=2) -> int:
+    # todo: make this handle full datetimes better. Like 2021-05-13 12:09:53
+    #  current behavior is to fail to match "2021-05-13" because it's not "2021-05-13 12:09:53", for example
     # returns row value of cell containing specified date, in specified column
     # returns -1 if not found
     # takes parameter sheet: a valid sheet object in an xlsx file
-    # takes parameter datetime_date: the datetime date to search for in date_column
+    # takes parameter datetime_target: the datetime date to search for in date_column
     # takes parameter date_column: column in which to search for date
+
+    datetime_target = datetime_target.replace(hour=0, minute=0, second=0, microsecond=0)
 
     # this may be redundant. We can probably assume we'll get a proper sheet object
     if not isinstance(sheet, openpyxl.worksheet.worksheet.Worksheet):
         print(
             f'Invalid parameter: find_row_of_datecell_given_datetime did not receive a valid sheet, sheet type = {type(sheet)}')
         return -1
-    if not isinstance(datetime_date, datetime):
+    if not isinstance(datetime_target, datetime):
         print(
-            f'Invalid parameter: find_row_of_datecell_given_datetime did not receive a valid datetime_date. It received: {datetime_date}')
+            f'Invalid parameter: find_row_of_datecell_given_datetime did not receive a valid datetime_target. It received: {datetime_target}')
         return -1
 
     # find date cell matching the "date" parameter in the given sheet
@@ -93,12 +114,12 @@ def find_row_of_datecell_given_datetime(sheet, datetime_date, date_column=2):
         # break if too many empty cells found in place of dates.
         if isinstance(sheet.cell(row=r, column=date_column).value, datetime):
             empty_cell_count = 0
-            if sheet.cell(row=r, column=date_column).value == datetime_date:
+            if sheet.cell(row=r, column=date_column).value == datetime_target:
                 return r
 
             # if examined cell is distant from workout's date, jump closer
             # we assume continuity in file's date column: that there's no time gap between start and final date.
-            days_to_advance = (datetime_date - sheet.cell(row=r, column=date_column).value).days
+            days_to_advance = (datetime_target - sheet.cell(row=r, column=date_column).value).days
             if days_to_advance > 3:
                 r += days_to_advance - 2
         else:
@@ -109,6 +130,38 @@ def find_row_of_datecell_given_datetime(sheet, datetime_date, date_column=2):
             empty_cell_count += 1
             if empty_cell_count > 50:
                 return -1
+
+
+def return_first_empty_bodyweight_row(sheet, date_column=2, bodyweight_column=3):
+    # returns the integer row where:
+    # 1) there's a date column cell filled in
+    # 2) there's a bodyweights column cell that's empty
+    # 3) the previous row has a filled in date cell, and bodyweights cell (disregarding empty rows, e.g. at year's end)
+
+    today = datetime.now()
+    todays_row = find_row_of_datecell_given_datetime(sheet, today, date_column)
+
+    if sheet.cell(row=todays_row, column=bodyweight_column).value:
+        return todays_row
+
+    num_rows_to_check = 10000
+    first_occurrence = todays_row
+    for x in range(num_rows_to_check):
+        # search backwards
+        row = todays_row - x
+        try:
+            row_has_date = isinstance(sheet.cell(row=row, column=date_column).value, datetime)
+            row_has_bodyweight = isinstance(sheet.cell(row=row, column=bodyweight_column).value, (str, float, int))
+            if row_has_date and not row_has_bodyweight:
+                first_occurrence = row
+            elif row_has_bodyweight:
+                break
+        except IndexError as e:
+            raise ValueError(f"Failed to find empty bodyweight cell. Row index out of range. Exception {e}")
+
+    if x != num_rows_to_check:
+        return first_occurrence
+    raise ValueError(f"Failed to find empty bodyweight cell. Examined {num_rows_to_check} rows")
 
 
 def is_est_xx_mins_line(line):
