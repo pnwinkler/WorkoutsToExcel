f439ce7daf9154472bfaccb9dc243480f19d81c8
Philip Winkler <pwinkl01@dcs.bbk.ac.uk>
2021-11-02 21:40:37 +0100

    Remove now unnecessary parameters

--- utilities/params.py
+++ utilities/params.py
@@ -9,12 +9,6 @@ import os
 TARGET_PATH = os.path.join(os.path.sep, 'home/philip/Laptop-Desktop/enc_files/home/philip/PRIO/personal/medication_and_health', 'food_eaten_diet.xlsx')
 TARGET_SHEET = 'Bodyweight and workouts'
 
-# SOURCE_PATH is where Keep2Calc writes the data that it pulls from Google Keep, for internal use. This will be tidied up, then written to CLEANED_DATA_PATH.
-# CLEANED_DATA_PATH is where Keep2Calc will write the tidied output of SOURCE_PATH to, for you to verify. The idea is that you can delete non-workout data that the program has erroneously retrieved (if any) before execution resumes.
-# Both files will be deleted automatically before program termination.
-SOURCE_PATH = os.path.join(os.path.sep, 'home', 'philip', 'Desktop', 'keep2calc_source.txt')
-CLEANED_DATA_PATH = os.path.join(os.path.sep, 'home', 'philip', 'Desktop', 'keep2calc_source_CLEANED.txt')
-
 # if left as None, will default to "Keep2Calc.backups/" in same dir as target file
 BACKUP_FOLDER_NAME = None
 
